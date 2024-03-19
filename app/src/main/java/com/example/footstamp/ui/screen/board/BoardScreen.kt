b62773f5d49f57a3e243704de3aff3ebd0a5da93
sangil <fhemflrh60@gmail.com>
2024-03-19 22:35:43 +0900

    feat: 게시판 base UI 구성

--- app/src/main/java/com/example/footstamp/ui/screen/board/BoardScreen.kt
+++ app/src/main/java/com/example/footstamp/ui/screen/board/BoardScreen.kt
@@ -1,21 +1,84 @@
 package com.example.footstamp.ui.screen.board
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import com.example.footstamp.R
+import com.example.footstamp.data.model.Diary
+import com.example.footstamp.data.util.Formatter
 import com.example.footstamp.ui.base.BaseScreen
+import com.example.footstamp.ui.components.BodyText
+import com.example.footstamp.ui.components.LabelText
+import com.example.footstamp.ui.components.SpaceMaker
 import com.example.footstamp.ui.components.TopBar
+import com.example.footstamp.ui.theme.SubColor
 
 @Composable
-fun BoardScreen() {
+fun BoardScreen(boardViewModel: BoardViewModel = hiltViewModel()) {
     BaseScreen { paddingValue, screenWidth, screenHeight ->
+        val diaries by boardViewModel.diaries.collectAsState()
+
         Column(modifier = Modifier.fillMaxSize()) {
             TopBar(text = stringResource(R.string.screen_board), backgroundColor = Color.White)
+
+            BoardGridLayout(diaries = diaries)
+        }
+    }
+}
+
+@Composable
+fun BoardGridLayout(diaries: List<Diary>) {
+    LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+        items(diaries) { diary ->
+            BoardGridItem(diary = diary)
+        }
+    }
+}
+
+@Composable
+fun BoardGridItem(diary: Diary) {
+    Box(modifier = Modifier.padding(3.dp)) {
+        Card(
+            colors = CardColors(
+                Color.White,
+                Color.Transparent,
+                Color.Transparent,
+                Color.Transparent
+            ),
+            border = BorderStroke(0.1.dp, SubColor)
+        ) {
+            AsyncImage(
+                model = Formatter.convertStringToBitmap(diary.photoBitmapStrings[diary.thumbnail]),
+                contentDescription = null,
+                modifier = Modifier.padding(10.dp)
+            )
+            BodyText(
+                text = diary.title, color = Color.Black,
+                modifier = Modifier.padding(horizontal = 10.dp)
+            )
+            SpaceMaker(height = 3.dp)
+            LabelText(
+                text = diary.location.location, color = SubColor,
+                modifier = Modifier.padding(horizontal = 10.dp)
+            )
+            SpaceMaker(height = 3.dp)
         }
     }
 }
--- app/src/main/java/com/example/footstamp/ui/screen/board/BoardViewModel.kt
+++ app/src/main/java/com/example/footstamp/ui/screen/board/BoardViewModel.kt
@@ -1,7 +1,35 @@
 package com.example.footstamp.ui.screen.board
 
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.example.footstamp.data.model.Diary
+import com.example.footstamp.data.repository.DiaryRepository
 import com.example.footstamp.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class BoardViewModel: BaseViewModel() {
+@HiltViewModel
+class BoardViewModel @Inject constructor(
+    private val repository: DiaryRepository
+) : BaseViewModel() {
 
+    private val _diaries = MutableStateFlow<List<Diary>>(emptyList())
+    val diaries = _diaries.asStateFlow()
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getAll().distinctUntilChanged().collect { diaryList ->
+                if (diaryList.isEmpty()) {
+                    Log.d("TAG", "EMPTY")
+                } else {
+                    _diaries.value = diaryList
+                }
+            }
+        }
+    }
 }
